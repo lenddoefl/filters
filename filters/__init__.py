38c2f95e1de356f428b349b3e26bad0cab1aa96f
Phoenix Zerin <phoenix.zerin@centrality.ai>
2018-03-08 13:02:24 +1300

    Removed `filters.__all__`.
    The top-level namespace should contain a lot more than just those two symbols.

--- filters/__init__.py
+++ filters/__init__.py
@@ -3,11 +3,6 @@ from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
 
-__all__ = [
-    'ext',
-    '__version__',
-]
-
 # http://stackoverflow.com/a/2073599/
 from pkg_resources import require
 __version__ = require('filters')[0].version
